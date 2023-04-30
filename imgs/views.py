d13664cc9cf37deffc506c454e7b50d9a05575d6
Devansh Shah <86820517+shahdevansh28@users.noreply.github.com>
2023-04-30 08:13:30 +0530

    emd extraction remaining

--- imgs/views.py
+++ imgs/views.py
@@ -12,11 +12,13 @@ import pywt
 import matplotlib.pyplot as plt
 import matplotlib.image as img
 import types
+import random as r
+import PIL.Image as Img
 # from google.colab.patches import cv2_imshow
 from math import log
 
 lem = 0
-
+map = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25,'A':26,'B':27,'C':28,'D':29,'E':30,'F':31,'G':32,'H':33,'I':34,'J':35,'K':36,'L':37,'M':38,'N':39,'O':40,'P':41,'Q':42,'R':43,'S':44,'T':45,'U':46,'V':47,'W':48,'X':49,'Y':50,'Z':51,' ':52,',':53,'.':54,'0':55,'1':56,'2':57,'3':58,'4':59,'5':60,'6':61,'7':62,'8':63,'9':64}
 # Create your views here.
 
 
@@ -86,6 +88,8 @@ def split(list_a, chunk_size):
         yield list_a[i:i + chunk_size]
 
 # LSB
+
+
 def lsb_hidedata(image, secret_message):
     n_bytes = image.shape[0]*image.shape[1]*3 // 8
 
@@ -134,6 +138,8 @@ def lsb_showData(image):
     return decoded_data[:-5]
 
 # DWT
+
+
 def dwt_hidedata(image, secret_message):
     n_bytes = image.shape[0]*image.shape[1] // 8
     if len(secret_message) > n_bytes:
@@ -154,7 +160,131 @@ def dwt_hidedata(image, secret_message):
                 break
     return image
 
+
+def dwt_showData(image_e):
+  binary_data=""
+  for i in range(0,image_e.shape[0]):
+    for j in range(0,image_e.shape[1]):
+      # print(type(pixel))
+      p = messageToBinary(image_e[i][j])
+      # print(p[-1])
+      binary_data += p[-1]
+  all_bytes = [ binary_data[i:i+8] for i in range(0,len(binary_data),8)]
+  decoded_data=""
+  for byte in all_bytes:
+    # byte = int(byte)
+    # print(int(byte,2))
+    decoded_data = decoded_data + chr(int(byte,2))
+    # print(decoded_data)
+    if decoded_data[-5:] == "#####":
+      break
+  # print(all_bytes)
+  return decoded_data[:-5]
+
+def dwt_decode_text(cH_round_e):
+  text=dwt_showData(cH_round_e)
+  return text
+
 # EMD
+def EMD_Decode(image, encoded_image, n):
+    print(n)
+    Mes=[]
+    ar = encoded_image
+    im = np.array(ar)
+    h,w = im.shape
+    # sp = np.array((h,w))
+    # for i in range(0,h):
+    #   for j in range(0,w):
+    #     sp[i][j] = (im[i][j][0] << 16) + (im[i][j][1]<<8) + (im[i][j][2])
+    sp = np.int8(im)
+    p = np.int8(image)
+    r = p-sp
+    i=0
+    f=int
+    f=int
+    while(i<h-1):
+        j=0
+        f=0
+        while(j<w):
+            f = (f + (j+1)*sp[i][j])%(2*n+1)
+            j = j + 1
+        f = f%(2*n + 1)
+        m=0
+        s=0
+        while(m<w):
+            if(r[i][m] == 1):
+                s = m
+                d = (s + f)%(2*n + 1)
+            # print("Mes is ",d)
+                Mes.append(d)
+            elif(r[i][m] == -1):
+                s1 = (2*n+1)-m
+                s2 = (2*n+1-m-w)
+                while(s2<0):
+                    s2 = s2 + (2*n+1)
+                if(i != 0):
+                    # print("Mes is ",(s1+f)%(2*n+1))
+                    Mes.append((s1+f)%(2*n+1))
+                else:
+                    # print("Mes is ",(s1+f)%(2*n+1))
+                    Mes.append((s1+f)%(2*n+1))
+            m = m + 1
+        i = i + 1
+        Message = ""
+    for i in Mes:
+        for key in map:
+            if(map[key] == i):
+                Message = Message + str(key)
+    print("Message is : ", Message)
+    return Message
+
+def EMD_hide_data(image, secret_message,n):
+    ar = Img.open(image)
+    im = np.int8(ar)
+    h,w = im.shape
+    # print(h,w)
+    while(True):
+        if(n<=64 and n>=0):
+            n = n + 64
+            break
+        elif(n<0):
+            n = 0 - n
+        elif(n>64):
+            break
+    p = np.array(Img.open(image))
+    s = 0
+    d=[]
+    xi = secret_message
+    for c in range(len(xi)):
+        d.append(map[xi[c]])
+    i=0
+    f = int
+    while(i<(len(d))):
+        j=0
+        f=0
+        while(j<w):
+            f = (f + (j+1)*im[i][j])%(2*n+1)
+            j = j + 1
+        f = f%(2*n + 1)
+        s = int((d[i]-f)%(2*n+1))
+        while(s<0):
+            s = s + (2*n+1)
+        if(s <= n):
+            p[i][s] = p[i][s] + 1
+        elif(s > n):
+            if(2*n+1-s>w):
+                if( p[i+1][2*n+1-s-w] == 0 ):
+                    p[i+1][2*n+1-s-w] = 255
+                else:
+                    p[i+1][2*n+1-s-w] = p[i+1][2*n+1-s-w] - 1
+            else:
+                if( p[i][(2*n+1)-s] == 0 ):
+                    p[i][(2*n+1)-s] = 255
+                else:
+                    p[i][(2*n+1)-s] = p[i][(2*n+1)-s]-1
+        i = i + 1
+    # cv2.imwrite('Embedded.png',p)
+    return p
 # Put your functions related to the algorithm
 
 
@@ -397,7 +527,10 @@ def image_view(request):
             form.save()
             binary_data = messageToBinary(message)
             lem = len(binary_data)
+            request.session['emd_original_image'] = img.name
             request.session['dlen'] = lem
+            request.session['n_val'] = r.randint(1,500)
+            n_val = request.session['n_val']
 
             data = {}
             data['image'] = "media/images/"+img.name
@@ -429,7 +562,7 @@ def image_view(request):
                 # image = cv2.imread(data['image'])
                 cv2.imwrite("media/images/dwt_hidden.png", encoded_image)
                 data['image'] = "media/images/"+"dwt_hidden.png"
-            
+
             # PVD
             elif choice == 'pvd':
                 print("Pvd")
@@ -440,9 +573,16 @@ def image_view(request):
                 cv2.imwrite("media/images/pvd_hidden.png", encoded_image)
                 data['image'] = "media/images/"+"pvd_hidden.png"
             # EMD
-            #Here comes the embedding algorithm
+            # Here comes the embedding algorithm
             elif choice == 'emd':
                 print("emd")
+                print(data['image'])
+                image = Img.open(data['image'])
+                print("n_val is ", n_val)
+                print("image given to ... is", image)
+                encoded_image = EMD_hide_data(data['image'], data['msg'],n_val)
+                cv2.imwrite("media/images/emd_hidden.png", encoded_image)
+                data['image'] = "media/images/"+"emd_hidden.png"
 
         return render(request, 'stego.html', {'data': data})
     else:
@@ -460,40 +600,50 @@ def stego(request, data):
 
 
 def extraction(request):
-    
+
     if request.method == 'POST':
-        img=request.FILES.get('Img')
-        choice=request.POST.get('choice')
+        img = request.FILES.get('Img')
+        choice = request.POST.get('choice')
         print(choice)
-        imgn=img.name
-        stego="media/images/"+imgn
+        x = str(request.session.get('emd_original_image'))
+        emd_image="media/images/"+x
+        emd_I = Img.open(emd_image)
+        imgn = img.name
+        stego = "media/images/"+imgn
         # print(stego)
-        dlen=request.session.get('dlen')
+        dlen = request.session.get('dlen')
+        n_val=request.session.get('n_val')
         # print(dlen)
-        embedded_image=cv2.imread(stego)
+        # request.session['emd_original_image'] = img.name
+        embedded_image = cv2.imread(stego)
+        emd = Img.open(stego)
         # decoded_data=lsb_showData(embedded_image)
         # print("Decoded data in string : ",decoded_data)
-        data={}
+        data = {}
         # data['msg']=decoded_data
-        data['Img']=stego
+        data['Img'] = stego
 
         if choice == '0':
             print("LSB")
-            decoded_data=lsb_showData(embedded_image)
-            data['msg']=decoded_data
-        
+            decoded_data = lsb_showData(embedded_image)
+            data['msg'] = decoded_data
+
         elif choice == '1':
             print("DWT")
-
+        
         elif choice == '2':
             print("PVD")
-            decoded_data=pvd_decode_data(embedded_image,dlen)
+            decoded_data = pvd_decode_data(embedded_image, dlen)
             data['msg'] = decoded_data
-        
+
         elif choice == '3':
             print("EMD")
-
-        return render(request ,'Actualmsg.html', {'data' : data })
+            decoded_data = EMD_Decode(emd,emd_I,n_val)
+            print("Decoded data in string : ", decoded_data)
+            data = {}
+            data['msg'] = decoded_data
+            data['Img'] = stego
+        return render(request, 'Actualmsg.html', {'data': data})
 
     else:
         return render(request, 'extraction.html')
